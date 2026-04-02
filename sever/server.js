d9f433d782da88228e5adfce095a77a4d4e0721c
Huynh Nhat Hoa <Hoa.hn22659@sinhvien.hoasen.edu.vn>
2026-04-02 12:54:32 +0700

    Bản server.js hoàn hảo nhất lịch sử

--- sever/server.js
+++ sever/server.js
@@ -79,7 +79,7 @@ app.get("/api/users", async (req, res) => {
 app.use(express.static(path.join(__dirname, "..", "client", "build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, ".", "client", "build", "index.html"));
 });
 
 // ================== SOCKET.IO ==================
