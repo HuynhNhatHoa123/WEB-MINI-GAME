eb60a21a8861210101305f026aa249d3e44e743b
Huynh Nhat Hoa <Hoa.hn22659@sinhvien.hoasen.edu.vn>
2026-04-02 09:26:48 +0700

    Sua socket de chay local va Render

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,22 @@
 import { io } from "socket.io-client";
 
-// Khi để trống hoặc dùng window.location.origin, 
-// Socket sẽ tự tìm đến địa chỉ của trang web hiện tại
-const socket = io(); 
+const socket = io(
+  process.env.NODE_ENV === "production"
+    ? undefined
+    : "http://localhost:5000"
+);
 
 export default socket;
+//ten nguoi dung mongodb huynhnhathoa22   pass : KIkOEJjUnw7qnq9i
+
+
+//IP Access List
+
+
+// Description
+
+
+// 58.187.191.72/32
+
+
+// Created as part of the Auto Setup process
